1a43f7397824a904eed6a7cad5bd055e68835adc
yuiwong <yuiwong@126.com>
2015-10-04 08:56:49 +0800

    add T

--- util/BufferUtil.swift
+++ util/BufferUtil.swift
@@ -1,6 +1,18 @@
 import Foundation
 
+class BufferAnyUtil <T> {
+	static func getBuffer (count c: Int, repeatedValue: T) -> [T] {
+		return [T](count: c, repeatedValue: repeatedValue)
+	}
+}
+
+
 class BufferUtil {
+	static func getBuffer (void_count c: Int) -> [Void] {
+		return [Void](count: c, repeatedValue: Void())
+	}
+
+
 	static func getBuffer (uint8_count c: Int) -> [UInt8] {
 		return [UInt8](count: c, repeatedValue: 0x0)
 	}
@@ -11,13 +23,28 @@ class BufferUtil {
 	}
 
 
+	static func getBuffer (int32_count c: Int) -> [Int32] {
+		return [Int32](count: c, repeatedValue: 0x0)
+	}
+
+
 	static func getBuffer (int64_count c: Int) -> [Int64] {
 		return [Int64](count: c, repeatedValue: 0x0)
 	}
 
 
+	static func getBuffer (uint64_count c: Int) -> [UInt64] {
+		return [UInt64](count: c, repeatedValue: 0x0)
+	}
+
+
 	static func getBuffer (char_count c: Int) -> [Character] {
 		return [Character](count: c, repeatedValue: "\0".characters.first!)
+	}
+
+
+	static func getBuffer (pthread_t_count c: Int) -> [pthread_t] {
+		return [pthread_t](count: c, repeatedValue: pthread_t())
 	}
 
 
