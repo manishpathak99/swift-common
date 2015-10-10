6826f363b7861c60c87ee6c1503e34a61ac38a4b
yuiwong <yuiwong@126.com>
2015-10-10 11:20:33 +0800

    nice show

--- util/LogUtil.swift
+++ util/LogUtil.swift
@@ -10,6 +10,7 @@ public class LogUtil {
 #if 	ENABLE_LOG
 		if (LogUtil.T >= LogUtil.logLevel) {
 			let t: String = name + " +\(line) " + funcName
+
 			LogUtil.log(tag: LogUtil.TAG_T, items: t)
 		}
 #endif
@@ -27,24 +28,17 @@ public class LogUtil {
 		if (LogUtil.V >= LogUtil.logLevel) {
 			let trace: String = name + " +\(line) " + funcName
 
-			LogUtil.log(tag: t, items: LogUtil.TAG_V, items, trace)
-		}
-#endif
+			var msg: String = LogUtil.TAG_V
+			if let tt = t {
+				msg += " " + tt
+			}
 
-	}
+			for i in items {
+				msg += " \(i)"
+			}
+			msg += " ." + trace
 
-
-	public static func i (
-		let name: String = __FILE__,
-		let line: Int = __LINE__,
-		let funcName: String = __FUNCTION__,
-		tag t: String?, items: Any ...) {
-
-#if 	ENABLE_LOG
-		if (LogUtil.I >= LogUtil.logLevel) {
-			let trace: String = name + " +\(line) " + funcName
-
-			LogUtil.log(tag: t, items: LogUtil.TAG_I, items, trace)
+			LogUtil.log(msg)
 		}
 #endif
 
@@ -61,7 +55,98 @@ public class LogUtil {
 		if (LogUtil.T >= LogUtil.logLevel) {
 			let trace: String = name + " +\(line) " + funcName
 
-			LogUtil.log(tag: t, items: LogUtil.TAG_T, trace, items)
+			var msg: String = LogUtil.TAG_T
+			if let tt = t {
+				msg += " " + tt
+			}
+
+			for i in items {
+				msg += " \(i)"
+			}
+			msg += " ." + trace
+
+			LogUtil.log(msg)
+		}
+#endif
+
+	}
+
+
+	public static func i (
+		let name: String = __FILE__,
+		let line: Int = __LINE__,
+		let funcName: String = __FUNCTION__,
+		tag t: String?, items: Any ...) {
+
+#if 	ENABLE_LOG
+		if (LogUtil.I >= LogUtil.logLevel) {
+			let trace: String = name + " +\(line) " + funcName
+
+			var msg: String = LogUtil.TAG_I
+			if let tt = t {
+				msg += " " + tt
+			}
+
+			for i in items {
+				msg += " \(i)"
+			}
+			msg += " ." + trace
+
+			LogUtil.log(msg)
+		}
+#endif
+
+	}
+
+
+	public static func w (
+		let name: String = __FILE__,
+		let line: Int = __LINE__,
+		let funcName: String = __FUNCTION__,
+		tag t: String?, items: Any ...) {
+
+#if 	ENABLE_LOG
+		if (LogUtil.I >= LogUtil.logLevel) {
+			let trace: String = name + " +\(line) " + funcName
+
+			var msg: String = LogUtil.TAG_W
+			if let tt = t {
+				msg += " " + tt
+			}
+
+			for i in items {
+				msg += " \(i)"
+			}
+			msg += " ." + trace
+
+			LogUtil.log(msg)
+		}
+#endif
+
+	}
+
+
+	public static func e (
+		let name: String = __FILE__,
+		let line: Int = __LINE__,
+		let funcName: String = __FUNCTION__,
+		tag t: String?, items: Any ...) {
+
+#if 	ENABLE_LOG
+		if (LogUtil.I >= LogUtil.logLevel) {
+			let trace: String = name + " +\(line) " + funcName
+
+			var msg: String = LogUtil.TAG_E
+			if let tt = t {
+				msg += " " + tt
+			}
+
+			for i in items {
+				msg += " \(i)"
+			}
+			msg += " ." + trace
+
+			LogUtil.log(msg)
 		}
 #endif
 
@@ -74,11 +159,17 @@ public class LogUtil {
 		if (l >= LogUtil.logLevel) {
 			let ts = TimeUtil.timestampstring()
 
-			if (nil != tag) {
-				print(ts, tag!, items)
-			} else {
-				print(ts, items)
+			var msg = ts
+
+			if let t = tag {
+				msg += " " + t
 			}
+
+			for i in items {
+				msg += " \(i)"
+			}
+
+			print(msg)
 		}
 #endif
 
@@ -90,11 +181,17 @@ public class LogUtil {
 #if 	ENABLE_LOG
 		let ts = TimeUtil.timestampstring()
 
-		if (nil != t) {
-			print(ts, t!, items)
-		} else {
-			print(ts, items)
+		var msg = ts
+
+		if let tt = t {
+			msg += " " + tt
 		}
+
+		for i in items {
+			msg += " \(i)"
+		}
+
+		print(msg)
 #endif
 
 	}
@@ -105,7 +202,13 @@ public class LogUtil {
 #if 	ENABLE_LOG
 		let ts = TimeUtil.timestampstring()
 
-		print(ts, items)
+		var msg = ts
+
+		for i in items {
+			msg += " \(i)"
+		}
+
+		print(msg)
 #endif
 
 	}
@@ -121,10 +224,18 @@ public class LogUtil {
 
 
 	public static let V: Int = 8
+	public static let D: Int = 9
 	public static let T: Int = 16
 	public static let I: Int = 24
-	public static let TAG_V: String = "VERBOSE"
-	public static let TAG_T: String = "TRACE"
-	public static let TAG_I: String = "INFO"
+	public static let W: Int = 32
+	public static let E: Int = 40
+	public static let F: Int = 48
+	public static let TAG_V: String = "lvVERBOSE"
+	public static let TAG_D: String = "lvDEBUG"
+	public static let TAG_T: String = "lvTRACE"
+	public static let TAG_I: String = "lvINFO"
+	public static let TAG_W: String = "lvWARN"
+	public static let TAG_E: String = "lvERROR"
+	public static let TAG_F: String = "lvFATAL"
 	public static var logLevel: Int = LogUtil.V
 }
